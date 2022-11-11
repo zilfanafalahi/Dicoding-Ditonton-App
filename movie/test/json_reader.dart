152a80f0cca36b4221c4349635bcaa76dde5670a
Zilfana Falahi <zilfanafalahi45@gmail.com>
2022-11-11 16:34:14 +0700

    Fix issue JsonReader & setting test arguments Codemagic

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('/movie/test')) {
+    dir = dir.replaceAll('/movie/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/movie/test/$name').readAsStringSync();
 }
